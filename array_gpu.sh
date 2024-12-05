939b2ca01ed8303fa82bb5842d137d67764e8720
merle <jgershon@uw.edu>
2024-12-05 12:49:22 -0800

    pushing array setup

---
+++ array_gpu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -p gpu
+#SBATCH --mem=8g
+#SBATCH --gres=gpu:a4000:1
+#SBATCH -c 1
+#SBATCH -o logs/ajob_%j_%a.out
+#SBATCH -J ajob
+#SBATCH -t 10:00:00
+#source activate /software/conda/envs/SE3
+# get line number ${SLURM_ARRAY_TASK_ID} from tasks file
+CMD=$(sed -n "${SLURM_ARRAY_TASK_ID}p" tasks)
+# tell bash to run $CMD
+echo "${CMD}" | bash
+
+#sbatch -a 1-$(cat tasks|wc -l) array_gpu.sh
