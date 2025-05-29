13d7f11e66b0a3937eeb1e88b32ed5446746afc3
merle <jgershon@uw.edu>
2025-05-29 14:56:41 -0700

    pushing

---
+++ reinforced_mpnn/train_policy_local.sh
@@ -0,0 +1,8 @@
+export MASTER_PORT=50067
+
+export PYTHONPATH=$PYTHONPATH:/home/jgershon/git/cleo/reinforced_mpnn/:/home/jgershon/git/cleo/
+export HYDRA_FULL_ERROR=1
+
+# apptainer -s run --nv /software/containers/mlfold.sif train_policy.py -cn penicillin_vanilla_pg
+
+source activate newlatent; python train_policy.py -cn penicillin_vanilla_pg
