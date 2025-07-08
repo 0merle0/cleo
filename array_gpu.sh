11d585251b2fdef40690868b529ac255c047bd2f
merle <jgershon@uw.edu>
2025-07-07 22:57:22 -0700

    setup opt loop

--- array_gpu.sh
+++ array_gpu.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-#SBATCH -p gpu
-#SBATCH --mem=48g
-#SBATCH --gres=gpu:a4000:1
+#SBATCH -p gpu-train
+#SBATCH --mem=128g
+#SBATCH --gres=gpu:h200:1
 #SBATCH -c 8
 #SBATCH -o logs/ajob_%j_%a.out
 #SBATCH -J ajob
-#SBATCH -t 10:00:00
-#source activate /software/conda/envs/SE3
+#SBATCH -t 2-00:00:00
 # get line number ${SLURM_ARRAY_TASK_ID} from tasks file
 CMD=$(sed -n "${SLURM_ARRAY_TASK_ID}p" tasks)
 # tell bash to run $CMD
