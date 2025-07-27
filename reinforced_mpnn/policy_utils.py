bca5c35b8ab8f8e2457c54479afbabfdae4ac904
merle <jgershon@uw.edu>
2025-07-27 09:28:01 -0700

    fixed checkpoint bug

--- reinforced_mpnn/policy_utils.py
+++ reinforced_mpnn/policy_utils.py
@@ -431,7 +431,7 @@ class PolicyMPNN:
             self.log_metrics(step, runtime, to_log)
 
             # model checkpointing
-            if step > 0 and self.checkpoint_every_n_steps % step == 0:
+            if step > 0 and  step % self.checkpoint_every_n_steps == 0:
                 self.checkpoint_model(step, to_log)
         
         print("Training complete.")
