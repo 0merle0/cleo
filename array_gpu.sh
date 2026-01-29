c7bbdfd8715e558c5274c7d46b9d8898a36419d5
merle <jgershon@uw.edu>
2026-01-29 06:18:05 -0800

    pushing to update before remaking main

--- array_gpu.sh
+++ array_gpu.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH -p gpu-train
+#SBATCH -p gpu
 #SBATCH --nodes=1
-#SBATCH --mem=128g
-#SBATCH --gres=gpu:h200:1
-#SBATCH -c 8
+#SBATCH --mem=16g
+#SBATCH --gres=gpu:a4000:1
+#SBATCH -c 1
 #SBATCH -o logs/ajob_%j_%a.out
 #SBATCH -J ajob
-#SBATCH -t 2-00:00:00
+#SBATCH -t 08:00:00
 # get line number ${SLURM_ARRAY_TASK_ID} from tasks file
 CMD=$(sed -n "${SLURM_ARRAY_TASK_ID}p" tasks)
 # tell bash to run $CMD
